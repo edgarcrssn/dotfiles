15fe2d6ac519e3efc836ba8345026c2442c4e3ba
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2022-07-31 16:52:29 +0200

    Add conditionnal to setup_zsh to check the shell before changing it

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,8 +2,20 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+# echo "Enter superuser (sudo) password to symlink sh to zsh"
+# sudo ln -sfv /bin/zsh /private/var/select/sh
