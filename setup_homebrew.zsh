99c2fe171a4c487b7c5949e19acea7051e3a1891
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2022-08-01 17:31:57 +0200

    Refined echo's
    Add some emojis

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,12 +1,15 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting Homebrew Setup >>>\n"
+echo "\n<<< 🍺 Starting Homebrew Setup 🍺 >>>\n"
 
 if exists brew; then
-  echo "Homebrew is already installed."
+  echo "ℹ️  Homebrew is already installed.\n"
 else
-  echo "Installing Homebrew..."
+  echo "⏳ Installing Homebrew...\n"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+echo "⏳ Installing packages and applications using brew..."
 brew bundle --verbose
+
+echo "\n<<< 🍺 Ending Homebrew Setup 🍺 >>>\n"
--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -1,30 +1,32 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting ZSH Setup >>>\n"
+echo "\n<<< 🤖 Starting ZSH Setup 🤖 >>>\n"
 
 # https://stackoverflow.com/a/4749368/1341838
 if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
-  echo '/opt/homebrew/bin/zsh already exists in /etc/shells.'
+  echo 'ℹ️  /opt/homebrew/bin/zsh already exists in /etc/shells.'
 else
-  echo "Enter superuser (sudo) password to edit /etc/shells:"
+  echo "⏳ Enter superuser (sudo) password to edit /etc/shells:"
   echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
-  echo '$SHELL is already /opt/homebrew/bin/zsh.'
+  echo 'ℹ️  $SHELL is already /opt/homebrew/bin/zsh.'
 else
-  echo "Enter user password to change login shell:"
+  echo "⏳ Enter user password to change login shell:"
   chsh -s '/opt/homebrew/bin/zsh'
 fi
 
 
 if sh --version | grep -q zsh; then
-  echo '/private/var/select/sh is already linked to /bin/zsh.'
+  echo 'ℹ️  /private/var/select/sh is already linked to /bin/zsh.'
 else
-  echo "Enter superuser (sudo) password to symlink sh to zsh:"
+  echo "⏳ Enter superuser (sudo) password to symlink sh to zsh:"
   # Looked cute, might delete later, idk
   sudo ln -sfv /bin/zsh /private/var/select/sh
 
   # I'd like for this to work instead.
   # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
 fi
+
+echo "\n<<< 🤖 Ending ZSH Setup 🤖 >>>\n"
