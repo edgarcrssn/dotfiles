02abeaf1338e4b4dabaf657703dc4c8615a048dc
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2022-08-05 19:03:39 +0200

    Made a file that explain how to set up the rest
    I prefer launch setup files manually than launch all files at once

---
+++ explanation.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+echo '\n✅ Dotfiles are set up !'
+
+echo '\nYou can now set up (respect the order):'
+  echo '\n\t🍺 Homebrew:'
+    echo '\t\t./setup_homebrew.zsh'
+  echo '\n\t🤖 zsh:'
+    echo '\t\t./setup_zsh.zsh'
+  echo '\n\t💎 Node:'
+    echo '\t\t./setup_node.zsh'
+  echo '\n\t💻 MacOS:'
+    echo '\t\t./setup_macos.zsh\n'
